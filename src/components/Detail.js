3c508cc483174d68fc75c021022732d427e1b6d8
ftees <tiennp8@gmail.com>
2021-06-27 20:36:14 +0700

    update button

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,6 +2,7 @@ import React from "react";
 import {useState, useEffect } from 'react';
 const Detail = (props) => {
   const [detail, setDetail] = useState({});
+  const [error, setError] = useState('');
   const id = props.idPara;
   const getMovieDetail = async (id) =>{
       const response = await fetch(`https://www.omdbapi.com/?i=${id}&apikey=c9127ecc`)
@@ -11,6 +12,10 @@ const Detail = (props) => {
   useEffect(()=>{
     getMovieDetail(id);
   },[detail])
+
+  const showError = (message) =>{
+     setError(message);
+  }
   return (
     <div className="container-fluid d-flex mt-4 mb-4">
       <img src={detail.Poster}></img>
@@ -25,8 +30,9 @@ const Detail = (props) => {
       <p>Language: {detail.Language}</p>
       <p>Runtime: {detail.Runtime}</p>
       <p>Released: {detail.Released}</p>
-      <button className="btn btn-primary"><b>Watch Movie</b></button>
-      <button className="btn btn-danger"><b>Download</b></button>
+      <button className="btn btn-primary" onClick={() =>showError("Watch function is not supported yet")}><b>Watch Movie</b></button>
+      <button className="btn btn-danger" onClick={() =>showError("Download function is not supported yet")}><b>Download</b></button>
+      <p className="error">{error}</p>
       </div>
 </div>
   );
